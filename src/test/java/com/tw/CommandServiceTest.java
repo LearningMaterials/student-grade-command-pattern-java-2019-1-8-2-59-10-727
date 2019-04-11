3c91265787fbfcf7cecc73d35d63f0a5d7dce7b0
mahuanfy <mahuanfy@foxmail.com>
2019-04-11 22:01:18 +0800

    Add user_select test

--- src/test/java/com/tw/CommandServiceTest.java
+++ src/test/java/com/tw/CommandServiceTest.java
@@ -19,4 +19,16 @@ public class CommandServiceTest {
                 "2. 生成成绩单\n" +
                 "3. 退出请输入你的选择（1～3）：", log.getLog().trim());
     }
+
+    @Test
+    public void user_select_one_menu (){
+        commandService.userSelect(1);
+        Assert.assertEquals("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：", log.getLog().trim());
+    }
+
+    @Test
+    public void user_select_Two_menu (){
+        commandService.userSelect(2);
+        Assert.assertEquals("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：", log.getLog().trim());
+    }
 }
