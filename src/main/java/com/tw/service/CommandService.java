dfe62f4c1e4d34bc9ae0c8ea9960d941ef707292
mahuanfy <mahuanfy@foxmail.com>
2019-04-11 22:03:11 +0800

    Finish add Student

--- src/main/java/com/tw/service/CommandService.java
+++ src/main/java/com/tw/service/CommandService.java
@@ -20,11 +20,35 @@ public class CommandService {
     public void userSelect(int menuNumber) {
         if (menuNumber == 1) {
             commandPage.inputStudentInfoPage();
+            addStudent();
         } else if (menuNumber == 2) {
             commandPage.inputStudentIdPage();
         } else if (menuNumber == 3) {
             System.exit(1);
         }
+    }
+
+    private void addStudent() {
+        Student student = new Student();
+        String studentInfo = input.nextLine();
+        String[] studentArray = studentInfo.split(", ");
+        if (studentArray.length < 3) {
+            commandPage.inputStudentInfoAgainPage();
+            addStudent();
+        } else {
+            student.setName(studentArray[0]);
+            student.setStudentId(studentArray[1]);
+            for (int i = 2; i < studentArray.length; i++) {
+                Subject subject = new Subject();
+                String[] subjectInfo = studentArray[i].split(": ");
+                subject.setName(subjectInfo[0]);
+                subject.setScore(Double.valueOf(subjectInfo[1]));
+                student.setSubject(subject);
+            }
+            studentData.add(student);
+            commandPage.inputStudentSuccessPage(student.getName());
+        }
+
     }
 
 }
